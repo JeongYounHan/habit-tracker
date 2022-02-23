ec4f333529553ef7c9a22192ab6bd5195e97916a
JeongYounHan <51sarang@hanmail.net>
2022-02-23 20:12:06 +0900

    habits 컴포넌트 작성

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,30 +1,43 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
+  // state = {
+  //   count: 0,
+  // };
+  // handleIncrement = () => {
+  //   // setState 사용해야 리액트가 데이터 변화 인지
+  //   this.setState({ count: this.state.count + 1 });
+  // };
+  // handleDecrement = () => {
+  //   // setState 사용해야 리액트가 데이터 변화 인지
+  //   const count = this.state.count - 1;
+  //   this.setState({ count: count < 0 ? 0 : count });
+  // };
+
   handleIncrement = () => {
-    // setState 사용해야 리액트가 데이터 변화 인지
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
   handleDecrement = () => {
-    // setState 사용해야 리액트가 데이터 변화 인지
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
   };
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
   render() {
+    // console.log(this.props.habit);
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button className="habit-button" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square habit-increase"></i>
         </button>
         <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
           <i className="fas fa-trash"></i>
         </button>
       </li>
